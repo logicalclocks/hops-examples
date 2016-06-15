3d33e4f5407fa6e8587d257d9e5f3695ae987e34
Theofilos Kakantousis <t.kakantousis@gmail.com>
2016-06-15 19:53:55 +0200

    Minor fix to allow consumer to print messages.

--- examples-kafka/src/main/java/io/hops/examples/kafka/KafkaHelloWorld.java
+++ examples-kafka/src/main/java/io/hops/examples/kafka/KafkaHelloWorld.java
@@ -3,6 +3,8 @@ package io.hops.examples.kafka;
 import io.hops.kafka.HopsKafkaConsumer;
 import io.hops.kafka.HopsKafkaProducer;
 import io.hops.kafka.HopsKafkaUtil;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 
@@ -17,7 +19,7 @@ public class KafkaHelloWorld {
     String type = null;
     int numberOfMessages = 30;
 
-        //Check user args
+    //Check user args
         /*
      * USAGES: 1. topicname numberOfMessages type(producer/consumer)
      * IF TYPE IS NOT PROVIDED, application will do both
@@ -28,7 +30,7 @@ public class KafkaHelloWorld {
       topicName = args[0];
       type = args[1];
       numberOfMessages = Integer.parseInt(args[2]);
-    } else if (args != null && args.length == 2 && args[1].equals("consumer") ) {
+    } else if (args != null && args.length == 2 && args[1].equals("consumer")) {
       topicName = args[0];
       type = args[1];
     } else if (args != null && args.length == 1) {
@@ -42,7 +44,7 @@ public class KafkaHelloWorld {
     //detects the app as succeeded      
     SparkConf sparkConf = new SparkConf().setAppName("Kafka");
     JavaSparkContext jsc = new JavaSparkContext(sparkConf);
-    
+
     //Setup the HopsKafkaUtil
     HopsKafkaUtil hopsKafkaUtil = HopsKafkaUtil.getInstance();
     hopsKafkaUtil.setup(topicName);
@@ -54,25 +56,42 @@ public class KafkaHelloWorld {
       //Produce Kafka messages to topic
       HopsKafkaProducer hopsKafkaProducer = new HopsKafkaProducer(topicName,
               false, numberOfMessages);
-      hopsKafkaProducer.run();
+      Map<String, Object> message;
+      for (int i = 0; i < numberOfMessages; i++) {
+        message = new HashMap<>();
+        message.put("firstname", "Henrik" + i);
+        message.put("lastname", "Larsson" + i);
+        message.put("team", "Sweden");
+        hopsKafkaProducer.produce(message);
+        //Thread.sleep(250);
+        System.out.println("KafkaHelloWorld sending message:"+message);
+      }
     } else {
       if (type.equals("producer")) {
         //Produce Kafka messages to topic
         HopsKafkaProducer hopsKafkaProducer = new HopsKafkaProducer(topicName,
                 false, numberOfMessages);
-        hopsKafkaProducer.run();
+        Map<String, Object> message = null;
+        for (int i = 0; i < numberOfMessages; i++) {
+        message = new HashMap<>();
+        message.put("firstname", "Henrik" + i);
+        message.put("lastname", "Larsson" + i);
+        message.put("team", "Sweden");
+        hopsKafkaProducer.produce(message);
+          System.out.println("Record:");
+      }
       } else {
         //Consume kafka messages from topic
         HopsKafkaConsumer hopsKafkaConsumer = new HopsKafkaConsumer(topicName);
         hopsKafkaConsumer.start();
-                //Keep thread alive
+        //Keep thread alive
         //THIS WILL CAUSE THE JOB TO HANG. USER HAS TO MANUALLY STOP THE JOB.
         while (true) {
           Thread.sleep(1000);
         }
       }
     }
-
+    Thread.sleep(8000);
     //Stop Spark context
     jsc.stop();
 
