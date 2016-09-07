203ce37dea8143cfcdb9819693c1036b8b0b5778
Theofilos Kakantousis <t.kakantousis@gmail.com>
2016-09-07 14:04:29 +0300

    Fixed params to be extracted from ParameterTool

--- examples-kafka/src/main/java/io/hops/examples/kafka/FlinkKafkaStreamingExample.java
+++ examples-kafka/src/main/java/io/hops/examples/kafka/FlinkKafkaStreamingExample.java
@@ -1,15 +1,18 @@
 package io.hops.examples.kafka;
 
 import io.hops.kafkautil.HopsKafkaUtil;
+import io.hops.kafkautil.flink.HopsAvroSchema;
 import io.hops.kafkautil.flink.HopsFlinkKafkaProducer;
+import java.util.Arrays;
 import java.util.Map;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.connectors.fs.DateTimeBucketer;
 import org.apache.flink.streaming.connectors.fs.RollingSink;
-import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 
 /**
  *
@@ -18,26 +21,27 @@ import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 public class FlinkKafkaStreamingExample {
 
   public static void main(String[] args) throws Exception {
-//		ParameterTool parameterTool = ParameterTool.fromArgs(args);
-//		if(parameterTool.getNumberOfParameters() < 2) {
-//			System.out.println("Missing parameters!\nUsage: Kafka --topic <topic> --bootstrap.servers <kafka brokers>");
-//			System.exit(1);
-//		}
+    ParameterTool parameterTool = ParameterTool.fromArgs(args);
+    if (parameterTool.getNumberOfParameters() < 2 || !parameterTool.has("topic")) {
+      System.out.println(
+              "Missing parameters!\nUsage: --topic <topic> --sink_path <rolling sink path>");
+      System.exit(1);
+    }
+    System.out.println("FlinkKafkaStreamingExample.Params:"+parameterTool.toMap().toString());
     HopsKafkaUtil hopsKafkaUtil = HopsKafkaUtil.getInstance();
     Map<String, String> kafkaProps = hopsKafkaUtil.getKafkaProps(
-            args[args.length - 1]);
+            parameterTool.get("kafka_params"));
     hopsKafkaUtil.setup(kafkaProps.get(hopsKafkaUtil.KAFKA_SESSIONID_ENV_VAR),
             Integer.parseInt(kafkaProps.get(
                     hopsKafkaUtil.KAFKA_PROJECTID_ENV_VAR)),
-            args[0],
+            parameterTool.get("topic"),
             "localhost",
             kafkaProps.get(hopsKafkaUtil.KAFKA_BROKERADDR_ENV_VAR),
             "http://localhost:8080",
-            "/srv/glassfish/domain1/config/" + kafkaProps.get(
-                    hopsKafkaUtil.KAFKA_K_CERTIFICATE_ENV_VAR),
-            "/srv/glassfish/domain1/config/" + kafkaProps.get(
-                    hopsKafkaUtil.KAFKA_T_CERTIFICATE_ENV_VAR));
-    if (args[1].equalsIgnoreCase("producer")) {
+            kafkaProps.get(hopsKafkaUtil.KAFKA_K_CERTIFICATE_ENV_VAR),
+            kafkaProps.get(hopsKafkaUtil.KAFKA_T_CERTIFICATE_ENV_VAR));
+
+    if (parameterTool.get("type").equalsIgnoreCase("producer")) {
       StreamExecutionEnvironment env = StreamExecutionEnvironment.
               getExecutionEnvironment();
       env.getConfig().disableSysoutLogging();
@@ -45,53 +49,61 @@ public class FlinkKafkaStreamingExample {
               10000));
 
       // very simple data generator
-      DataStream<String> messageStream = env.addSource(
-              new SourceFunction<String>() {
-        public boolean running = true;
+      DataStream<Tuple4<String, String, String, String>> messageStream = env.
+              addSource(
+                      new SourceFunction<Tuple4<String, String, String, String>>() {
+                public boolean running = true;
 
-        @Override
-        public void run(SourceContext<String> ctx) throws Exception {
-          long i = 0;
-          while (this.running) {
-            ctx.collect("Element - " + i++);
-            Thread.sleep(500);
-          }
-        }
+                @Override
+                public void run(
+                        SourceContext<Tuple4<String, String, String, String>> ctx)
+                        throws Exception {
+                  long i = 0;
+                  while (this.running) {
+                    ctx.collect(new Tuple4("platform", "HopsWorks-" + i++,
+                            "program", "Flink Streaming"));
+                    Thread.sleep(500);
+                  }
+                }
 
-        @Override
-        public void cancel() {
-          running = false;
-        }
-      });
+                @Override
+                public void cancel() {
+                  running = false;
+                }
+              });
 
       // write data into Kafka
-      //messageStream.addSink(new FlinkKafkaProducer08<>(parameterTool.getRequired("topic"), new SimpleStringSchema(), parameterTool.getProperties()));
-      HopsFlinkKafkaProducer producer = new HopsFlinkKafkaProducer<>(args[0],
-              new SimpleStringSchema(), null);
+      HopsFlinkKafkaProducer producer = new HopsFlinkKafkaProducer(
+              parameterTool.get("topic"),
+              new HopsAvroSchema(parameterTool.get("topic")));
       messageStream.addSink(producer);
 
       env.execute("Write into Kafka example");
     } else {
-      
+
       StreamExecutionEnvironment env = StreamExecutionEnvironment.
               getExecutionEnvironment();
       env.getConfig().disableSysoutLogging();
       env.getConfig().setRestartStrategy(RestartStrategies.fixedDelayRestart(4,
               10000));
       //env.enableCheckpointing(5000); // create a checkpoint every 5 secodns
-      //env.getConfig().setGlobalJobParameters(parameterTool); // make parameters available in the web interface
-
-      DataStream<String> messageStream = env
-              .addSource(hopsKafkaUtil.getHopsFlinkKafkaConsumer(args[0],
-                      new SimpleStringSchema()));
-
+      //Get user parameters, excluding kafka ones set by HopsWorks
+      // make parameters available in the web interface
+      env.getConfig().setGlobalJobParameters(ParameterTool.fromArgs(
+              Arrays.copyOf(args, args.length-2))); 
       
-      RollingSink<String> rollingSink = new RollingSink<>("/Projects/Flink/Resources");
+      DataStream<String> messageStream = env
+              .addSource(hopsKafkaUtil.getHopsFlinkKafkaConsumer(parameterTool.
+                      get("topic"),
+                      new HopsAvroSchema(parameterTool.get("topic"))));
+
+      RollingSink<String> rollingSink = new RollingSink<>(
+              parameterTool.get("sink_path"));
       //Size of part file in bytes
-      rollingSink.setBatchSize(1024);
+      rollingSink.setBatchSize(1024 * 1024);
       rollingSink.setBucketer(new DateTimeBucketer("yyyy-MM-dd--HHmm"));
       messageStream.addSink(rollingSink);
-      
+
       // write kafka stream to standard out.
       messageStream.print();
 
