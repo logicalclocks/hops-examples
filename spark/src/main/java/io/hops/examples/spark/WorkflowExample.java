37625bddef41c3cea6c2efde262f3ca732262b72
Theofilos Kakantousis <t.kakantousis@gmail.com>
2018-01-22 23:45:21 +0100

    WorkflowExample comment clarification

--- spark/src/main/java/io/hops/examples/spark/WorkflowExample.java
+++ spark/src/main/java/io/hops/examples/spark/WorkflowExample.java
@@ -9,8 +9,9 @@ import javax.ws.rs.core.Response;
 import org.apache.spark.sql.SparkSession;
 
 /**
- * Example demo showing how to build Spark job workflows in Hopsworks. In particular, it expected that two Spark
- * jobs are already created in the project
+ * Example demo showing how to build Spark job workflows in Hopsworks. In particular, it expects that two Spark
+ * jobs are already created in the project. It then starts the first job, waits for it to finish and then starts the
+ * second one.
  * <p>
  */
 public class WorkflowExample {
@@ -24,16 +25,16 @@ public class WorkflowExample {
         .appName(HopsUtil.getJobName())
         .getOrCreate();
 
-    //Start job with ID: 6145 that prepares data for job with id 6146
+    //if Start job with given ID
     Response resp = WorkflowManager.startJobs(Integer.parseInt(args[0]));
     if (resp.getStatus() != Response.Status.OK.getStatusCode()) {
       LOG.log(Level.SEVERE, "Job could not be started");
       System.exit(1);
     }
-    //Wait for job with ID: 6145 to complete
+    //Wait for previous job to complete
     WorkflowManager.waitForJobs(Integer.parseInt(args[0]));
 
-    //Start job with ID: 6146
+    //Start second job
     WorkflowManager.startJobs(Integer.parseInt(args[1]));
 
     //Stop spark session
