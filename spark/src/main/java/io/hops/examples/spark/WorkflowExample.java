2338d3a1ea8743961fd0e9d599f64721ebc6ac4b
Theofilos Kakantousis <t.kakantousis@gmail.com>
2019-03-08 12:00:57 +0100

    Merge

--- spark/src/main/java/io/hops/examples/spark/WorkflowExample.java
+++ spark/src/main/java/io/hops/examples/spark/WorkflowExample.java
@@ -14,15 +14,12 @@
 
 package io.hops.examples.spark;
 
-import io.hops.util.exceptions.CredentialsNotFoundException;
 import io.hops.util.Hops;
 import io.hops.util.WorkflowManager;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.ws.rs.core.Response;
-
 import io.hops.util.exceptions.WorkflowManagerException;
 import org.apache.spark.sql.SparkSession;
+
+import java.util.logging.Logger;
 
 /**
  * Example demo showing how to build Spark job workflows in Hopsworks. In particular, it expects that two Spark
@@ -35,7 +32,7 @@ public class WorkflowExample {
   private static final Logger LOG = Logger.getLogger(WorkflowManager.class.getName());
 
   public static void main(String[] args)
-      throws CredentialsNotFoundException, InterruptedException, WorkflowManagerException {
+      throws InterruptedException, WorkflowManagerException {
 
     SparkSession spark = SparkSession
         .builder()
@@ -43,16 +40,13 @@ public class WorkflowExample {
         .getOrCreate();
 
     //if Start job with given ID
-    Response resp = WorkflowManager.startJobs(Integer.parseInt(args[0]));
-    if (resp.getStatus() != Response.Status.OK.getStatusCode()) {
-      LOG.log(Level.SEVERE, "Job could not be started");
-      System.exit(1);
-    }
+    WorkflowManager.startJobs(args[0]);
+
     //Wait for previous job to complete
-    WorkflowManager.waitForJobs(Integer.parseInt(args[0]));
+    WorkflowManager.waitForJobs(args[0]);
 
     //Start second job
-    WorkflowManager.startJobs(Integer.parseInt(args[1]));
+    WorkflowManager.startJobs(args[1]);
 
     //Stop spark session
     spark.stop();
