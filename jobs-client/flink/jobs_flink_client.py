846edf0a07b63290d868bda5c7e980eaa50fe775
Theofilos Kakantousis <t.kakantousis@gmail.com>
2020-07-15 14:51:44 +0200

    Improve client to wait for cluster startup

--- jobs-client/flink/jobs_flink_client.py
+++ jobs-client/flink/jobs_flink_client.py
@@ -2,19 +2,76 @@ import os
 import requests
 import argparse
 import urllib.parse
+import time
+from json.decoder import JSONDecodeError
+
+from hops import project, beam, jobs, util
+from hops.exceptions import APIKeyFileNotFound, RestAPIError
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--hopsworks-url", help="Hopsworks url (domain and port")
 parser.add_argument("-appid", "--application_id", help="The Flink job application id")
 parser.add_argument("-jar", help="The Flink job jar file")
+parser.add_argument("-p", "--project", help="The Hopsworks project name")
+parser.add_argument("-j", "--job", default='flinkcluster', help="The Hopsworks job name")
 parser.add_argument("-m", "--main", help="The entry point to the application, file with main function")
-parser.add_argument("-a", "--apikey", help="The file containing the API key to be used to "
-                                                                 "submit the job")
+parser.add_argument("-a", "--apikey", help="The file containing the API key to be used to connect to the project and "
+                                           "submit the job")
+parser.add_argument("-tm", "--task-managers", default="1", help="Number of Flink task managers")
+parser.add_argument("-yjm", "--yarnjobManagerMemory", default="2048", help="Memory of the Flink job manager in MB")
+parser.add_argument("-ytm", "--yarntaskManagerMemory", default="4096", help="Memory of the Flink task managers in MB")
+parser.add_argument("-ys", "--yarnslots", default="1", help="Number of slots per TaskManager")
 parser.add_argument("-parallelism", "--parallelism", default="1", help="Flink job parallelimsThe file containing the API key to be used to submit the job")
 parser.add_argument("-args", "--job-arguments", help="Flink job runtime arguments")
 args = parser.parse_args()
-print(args)
-base_url = "https://" + args.hopsworks_url + "/hopsworks-api/flinkmaster/" + args.application_id
+
+hopsworks_url = args.hopsworks_url.split(":")
+project.connect(args.project, hopsworks_url[0], port=hopsworks_url[1], api_key=args.apikey)
+
+# Check if Flink job with this name is already running
+execution = jobs.get_executions(args.job,
+                                "?filter_by=state:INITIALIZING,RUNNING,ACCEPTED,NEW,NEW_SAVING,SUBMITTED,"
+                                "STARTING_APP_MASTER,AGGREGATING_LOGS&sort_by=id:desc")
+app_id = args.application_id
+if execution is None or execution['count'] == 0:
+    # Create Flink Hopsworks job and start it. This effectively creates a Flink cluster to submit jobs to
+    beam.create_runner(args.job,
+                       args.yarnjobManagerMemory,
+                       args.task_managers,
+                       args.yarntaskManagerMemory,
+                       args.yarnslots)
+    print("Waiting for flink cluster to start...")
+    beam.start_runner(args.job)
+    # Wait 90 seconds until runner is in status "RUNNING",
+    wait = 90
+    wait_count = 0
+    execution = jobs.get_executions(args.job, "?offset=0&limit=1&sort_by=id:desc")['items'][0]
+    state = execution['state']
+    while wait_count < wait and state != "RUNNING":
+        time.sleep(5)
+        wait_count += 5
+        execution = jobs.get_executions(args.job, "?offset=0&limit=1&sort_by=id:desc")['items'][0]
+        state = execution['state']
+
+    if state != "RUNNING":
+        print("Flink cluster did not start, check job logs for details")
+        exit(1)
+    else:
+        app_id = execution['appId']
+        print("app_id: " + execution['appId'])
+        print("Flink cluster started successfully. Will now proceed to submit the job.")
+else:
+    app_id = execution['items'][0]['appId']
+    print("Found Flink cluster with this name already running, will use it to submit the Flink job")
+
+base_url = "https://" + args.hopsworks_url + "/hopsworks-api/flinkmaster/" + app_id
+print("base_url:" + base_url)
+
+try:
+    with open(args.apikey) as f:
+        args.apikey = f.readline().strip()
+except:
+    raise APIKeyFileNotFound('API Key file could not be read or was not found')
 
 # Upload Flink job jar to job manager
 response = requests.post(
@@ -28,10 +85,6 @@ response = requests.post(
     },
     headers={"Authorization": "Apikey " + args.apikey}
 )
-print(response.json())
-print(response.json()["filename"])
-
-# Request URL: https://8a4c06f0-c508-11ea-ac91-19f73699d396.aws.hopsworks.ai/hopsworks-api/flinkmaster/application_1594645575168_0002/jars/ca3e0287-e0f8-427b-971b-cc6314323346_WordCount.jar/run?entry-class=org.apache.flink.streaming.examples.wordcount.WordCount&parallelism=1&program-args=--arg1%20a1%20--arg2%20a2
 
 # Run job
 jar_id = response.json()["filename"].split("/")[-1]
@@ -39,10 +92,22 @@ print(jar_id)
 job_args = urllib.parse.quote(args.job_arguments)
 print("jobs_args:" + job_args)
 base_url += "/jars/" + jar_id + "/run?entry-class=" + args.main + "&program-args=" + job_args
-print(base_url)
+print("Submitting job to: " + base_url)
 
 response = requests.post(
     base_url,
     headers={"Content-Type" : "application/json", "Authorization": "Apikey " + args.apikey}
 )
-print(response)
+try:
+    response_object = response.json()
+except JSONDecodeError:
+    response_object = None
+if (response.status_code // 100) != 2:
+    if response_object:
+        error_code, error_msg, user_msg = util._parse_rest_error(response_object)
+    else:
+        error_code, error_msg, user_msg = "", "", ""
+
+    raise RestAPIError("Could not execute HTTP request (url: {}), server response: \n "
+                       "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
+        base_url, response.status_code, response.reason, error_code, error_msg, user_msg))
