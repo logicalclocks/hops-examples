5ff48f33d5fa635c1f727fe911690905847d9fd2
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2022-05-18 17:49:20 +0200

    [HOPSWORKS-3148] Update examples to use HSML

--- notebooks/ml/serving/kserve/sklearn/transformer.py
+++ notebooks/ml/serving/kserve/sklearn/transformer.py
@@ -1,12 +1,12 @@
 class Transformer(object):
     def __init__(self):
-        print("Initializing transformer")
-        # Initialization code goes here
+        """Initialization code goes here"""
+        # NOTE: The env var ARTIFACT_FILES_PATH contains the path to the model artifact files
 
     def preprocess(self, inputs):
-        # Transform the request inputs here. The object returned by this method will be used as model input.
+        """Transform the request inputs. The object returned by this method will be used as model input."""
         return inputs
 
     def postprocess(self, outputs):
-        # Transform the predictions computed by the model before returning a response.
+        """Transform the predictions computed by the model before returning a response."""
         return outputs
