9830cea651f2c5c403e9301bd3b1a6d311c59a28
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2022-05-21 07:22:08 +0200

    [HOPSWORKS-3148] Update examples to use HSML (#313)

--- notebooks/ml/serving/kserve/python/predictor.py
+++ notebooks/ml/serving/kserve/python/predictor.py
@@ -1,26 +1,15 @@
-import joblib
-from hops import hdfs
+import os
+from sklearn.externals import joblib
 
 class Predict(object):
 
     def __init__(self):
-        """Initializes the serving state, reads a trained model from HDFS"""
-        self.model_path = "Models/irisflowerclassifier/1/iris_knn.pkl"
-        print("Copying scikit-learn model from HDFS to local directory")
-        hdfs.copy_to_local(self.model_path)
-        print("Reading local scikit-learn model for serving")
-        self.model = joblib.load("./iris_knn.pkl")
-        print("Initialization complete")
-
+        """Prepare and load a trained model"""
+        # NOTE: The env var ARTIFACT_FILES_PATH contains the path to the model artifact files
+        
+        # load the trained model
+        self.model = joblib.load(os.environ["ARTIFACT_FILES_PATH"] + "/iris_knn.pkl")
 
     def predict(self, inputs):
-        """ Serves a prediction request using a trained model"""
+        """Serve prediction using a trained model"""
         return self.model.predict(inputs).tolist() # Numpy Arrays are note JSON serializable
-
-    def classify(self, inputs):
-        """ Serves a classification request using a trained model"""
-        return "not implemented"
-
-    def regress(self, inputs):
-        """ Serves a regression request using a trained model"""
-        return "not implemented"
